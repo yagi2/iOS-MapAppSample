3c3756ac3ab5b2e6fc11202139ebafbe34d17061
Itsuki Aoyagi <mail@yagi2.dev>
2021-05-23 23:29:34 +0900

    show map and search text field view

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        inputText.delegate = self
     }
-
-
+    
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var displayMap: MKMapView!
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // キーボードを閉じる
+        textField.resignFirstResponder()
+        
+        // 入力された文字を取り出す
+        if let searchKey = textField.text {
+            print(searchKey)
+        }
+        
+        return true
+    }
 }
 
