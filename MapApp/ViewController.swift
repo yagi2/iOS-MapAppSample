c594bd62691f8bc6661d47d444cfb887db671bc5
Itsuki Aoyagi <mail@yagi2.dev>
2021-05-23 23:37:10 +0900

    search markplace and pinned there

--- MapApp/ViewController.swift
+++ MapApp/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -26,9 +26,35 @@ class ViewController: UIViewController, UITextFieldDelegate {
         
         // 入力された文字を取り出す
         if let searchKey = textField.text {
-            print(searchKey)
+            let geocoder = CLGeocoder()
+
+            //入力された文字から位置情報を取得
+            geocoder.geocodeAddressString(searchKey, completionHandler: { (placemarks, error) in
+                // 位置情報が存在する場合は取り出す
+                if let unwrapPlacemarks = placemarks {
+                    // 1件目の情報を取り出す
+                    if let firstPlacemark = unwrapPlacemarks.first {
+                        // 位置情報を取り出す
+                        if let location = firstPlacemark.location {
+                            // 位置情報から緯度経度を取り出す
+                            let targetCoordinate = location.coordinate
+                            
+                            // マップにピンを置いていく
+                            let pin = MKPointAnnotation()
+                            pin.coordinate = targetCoordinate
+                            pin.title = searchKey
+                            
+                            self.displayMap.addAnnotation(pin
+                            )
+                            
+                            // 緯度経度を中心に500mの範囲を表示
+                            self.displayMap.region = MKCoordinateRegion(center: targetCoordinate, latitudinalMeters: 500.0, longitudinalMeters: 500.0)
+                        }
+                    }
+                }
+            })
+            
         }
-        
         return true
     }
 }
