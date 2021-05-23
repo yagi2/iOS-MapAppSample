8754c96d1091cdac7542f83785bfe4740798dc63
Itsuki Aoyagi <mail@yagi2.dev>
2021-05-23 23:06:02 +0900

    initial commit

---
+++ MapApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapApp
+//
+//  Created by Itsuki Aoyagi on 2021/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
